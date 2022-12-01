919b2994e52350c021f59107c55e630ae97e146e
chigari101 <chigari101@gmail.com>
2022-12-01 15:25:12 -0800

    added files on python importing & modules project

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
     i = len(sys.argv) - 1
-
 
     if i == 0:
         print("{} arguments.".format(i))
@@ -12,11 +10,8 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(i))
 
-
     if i >= 1:
-
         i = 0
-
         for arg in sys.argv:
             if i != 0:
                 print("{}: {}".format(i, arg))
