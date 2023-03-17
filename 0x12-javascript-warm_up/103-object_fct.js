7cd00d514f7a3bc385dde0da5042d701f3189bba
chigari101 <chigari101@gmail.com>
2023-03-17 11:42:01 -0700

    added files on warm_up project

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let myObject = {
+const myObject = {
   type: 'object',
   value: 12
 };
@@ -15,3 +15,4 @@ myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
+
