615d2c2885f5ed176eab319fdf8b71c80528b3d7
chigari101 <chigari101@gmail.com>
2023-03-22 13:45:20 +0000

    added files on python ORM project

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module that containes the class definition of a State and instance Base"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -14,4 +15,3 @@ class State(Base):
     id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
